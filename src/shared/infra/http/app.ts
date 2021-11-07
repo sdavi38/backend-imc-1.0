9869e849892dc8b01c9810c0c0bf8f4ceb5e6bcf
David Bernardo <sdavi28@hotmail.com>
2021-11-07 19:28:42 -0300

    build

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,4 +1,4 @@
-
+import 'dotenv/config';
 import '../typeorm';
 
 import express, { Request, Response, NextFunction } from 'express';
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,12 +1,10 @@
-import  dotenv from 'dotenv'
 import 'reflect-metadata';
-
-dotenv.config();
+ 
 
 import app from './app';
 
-const port = process.env.PORT || 3333;
+const port = process.env.DATABASE_URL ? process.env.PORT : 5000;
 
 app.listen(port, () => {
-  console.log(`🚀 Server started! is ${port}`);
+  console.log('🚀 Server started!');
 });
