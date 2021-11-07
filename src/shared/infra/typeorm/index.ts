043eda9aa6713c09bfc8173748a6272de2a3ab26
David Bernardo <sdavi28@hotmail.com>
2021-11-07 19:07:15 -0300

    build

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,40 @@
 import { createConnection } from 'typeorm';
+require("dotenv").config();
+const rootDir = process.env.NODE_ENV === "development" ?
+"src":
+"dist";
 
-createConnection();
+const extensionFile = process.env.NODE_ENV === "development" ?
+"ts":
+"js"; 
+
+const config: any = {
+
+  type: "postgres",
+  /* host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSOWRD,
+  database: process.env.TYPEORM_DATABASE, */
+  url: process.env.DATABASE_URL,
+  synchronize: false,
+  extra: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  entities: [
+    rootDir + `/modules/**/infra/typeorm/entities/*.${extensionFile}`],
+  migrations: [
+    rootDir + `/shared/infra/typeorm/migrations/*.${extensionFile}`],
+  cli: {
+    migrationsDir:
+    rootDir + "src/shared/infra/typeorm/migrations"
+  }
+}
+
+
+
+
+createConnection(config).catch(erro => console.log(erro));
